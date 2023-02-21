9129b581ed3095dccbf392ee58242bbbb5fccb8c
JoSuzuki <jonathan@suzuki.pro.br>
2023-02-20 23:22:54 -0300

    Update readme and add noticeable and missing features

--- README.md
+++ README.md
@@ -1,41 +1,5 @@
-# Welcome to Remix!
+# Welcome to Shine
 
-- [Remix Docs](https://remix.run/docs)
+This small app was built with [Remix](https://remix.run/docs). This was built as a mockup of [Company] to demonstrate how some acessibility features could be incorporated to enable a better experience by the users of the app.
 
-## Fly Setup
-
-1. [Install `flyctl`](https://fly.io/docs/getting-started/installing-flyctl/)
-
-2. Sign up and log in to Fly
-
-```sh
-flyctl auth signup
-```
-
-3. Setup Fly. It might ask if you want to deploy, say no since you haven't built the app yet.
-
-```sh
-flyctl launch
-```
-
-## Development
-
-From your terminal:
-
-```sh
-npm run dev
-```
-
-This starts your app in development mode, rebuilding assets on file changes.
-
-## Deployment
-
-If you've followed the setup instructions already, all you need to do is run this:
-
-```sh
-npm run deploy
-```
-
-You can run `flyctl info` to get the url and ip address of your server.
-
-Check out the [fly docs](https://fly.io/docs/getting-started/node/) for more information.
+To develop, run `npm run dev`.
--- app/routes/lessons/decision-trees.tsx
+++ app/routes/lessons/decision-trees.tsx
@@ -37,6 +37,51 @@ export default function DecisionTrees() {
     <Fragment>
       <h1>Decision trees</h1>
       <Spacer size="md" />
+      Cool features to notice in this example:
+      <ul>
+        <li>
+          The card maintains the size of the larger content (front or back).
+        </li>
+        <li>
+          When a screen reader reads the card, it only reads the content that is
+          being displayed. (Depending on the implementation it'd read the back
+          when only the front should be visible or would read the front when
+          only the back is visible)
+        </li>
+        <li>
+          When showing the explanation of a card the focus is moved to the top
+          of the card, allowing screen readers to follow the "natural flow",
+          instead of having to come back to the previous content
+        </li>
+        <li>You can select the alternatives (which face) via keyboard</li>
+        <li>
+          Screen readers display labels for the faces relative to the decision
+          tree necessary to get to the face
+        </li>
+        <li>Drag and drop is still implemented for the marker</li>
+        <li>
+          When clicking the check option it has a aria-live region which allows
+          a user with a screen reader to know what was the response of that
+          button press
+        </li>
+        <li>
+          Usage of role="radio-group" allows assistive tecnology to know that
+          the radio on the page belongs to a specific group (if not specified,
+          it usually considers all radios to be in one big group)
+        </li>
+      </ul>
+      Known missing features:
+      <ul>
+        <li>
+          Selecting the correct option doesn't disable the radio (you can change
+          the answer after selecting correct)
+        </li>
+        <li>There is no database, so no persistency</li>
+        <li>Doesn't show in the svg the selected path</li>
+        <li>No solution animations</li>
+        <li>There could be a better description of the svg as an alt text</li>
+      </ul>
+      <Spacer size="md" />
       <ExplainableCard
         front={<ExerciseDecisionTree01 />}
         back={<ExplanationExerciseDecisionTree01 />}
